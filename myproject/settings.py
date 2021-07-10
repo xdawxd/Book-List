2e6fd0580beb3af303fbdc7a4beb4bbe2fcc3d11
Dawid Schwinge <259639@student.pwr.edu.pl>
2021-07-10 21:15:30 +0200

    fixed some bugs

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['book-management-systm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
