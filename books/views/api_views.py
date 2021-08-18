714aeee6cf2758f29559af8797df0f4fe926d299
Dawid Schwinge <259639@student.pwr.edu.pl>
2021-08-18 15:03:35 +0200

    refactor: Refactored api_view

--- books/views/api_views.py
+++ books/views/api_views.py
@@ -8,6 +8,7 @@ from books.forms import SearchBookForm
 from django.shortcuts import render
 import requests
 from . import API_URL
+import pandas as pd
 from myproject.settings import env
 
 
@@ -27,40 +28,37 @@ class ImportBookView(View):
     form_class = SearchBookForm
     template_name = 'books/book_import.html'
 
+    def correct_date(self, pub_date):
+        return str(pd.to_datetime(pub_date)).split(' ')[0]
+
+    def correct_isbn(self, book):
+        return list(filter(
+            lambda k: k['type'] != 'ISBN_10', book['volumeInfo'].get('industryIdentifiers'))
+        )
+
     def get(self, request, *args, **kwargs):
         form = self.form_class(request.GET)
         return render(request, self.template_name, {'form': form})
 
-    def search(self, value, term):
+    def search(self, name, value, term):
         api_key = env('API_KEY')
-        params = f'q=Hobbit+{term}:{value}&key={api_key}'
+        params = f'q={name}+{term}:{value}&key={api_key}'  # TODO -> rewrite this hardcoded url
         books = requests.get(API_URL, params=params)
+        print(books.url)
         books_json = books.json()
-        return books_json['items'], books.status_code
+        return books_json['items']
 
     def add_books(self, books):
         for book in books:
-            isbn = list(filter(
-                lambda k: k['type'] != 'ISBN_10', book['volumeInfo'].get('industryIdentifiers'))
-            )
-            isbn13, = isbn
-
-            author = book['volumeInfo'].get('authors')
-            if author:
-                author = author[0]
-
-            p_date = book['volumeInfo'].get('publishedDate')
-
-            if len(p_date) == 4:
-                p_date += "-01-01"
-            elif len(p_date) == 7:
-                p_date += "-01"
+            pub_date = self.correct_date(book['volumeInfo'].get('publishedDate'))
+            author = book['volumeInfo'].get('authors')[-1]  # TODO -> add author model and fix this
+            isbn, = self.correct_isbn(book)
 
             Book.objects.get_or_create(
                 title=book['volumeInfo'].get('title'),
                 author=author,
-                pub_date=p_date,
-                isbn_num=isbn13['identifier'],
+                pub_date=pub_date,
+                isbn_num=isbn['identifier'],
                 page_count=book['volumeInfo'].get('pageCount'),
                 preview_link=book['volumeInfo'].get('previewLink'),
                 language=book['volumeInfo'].get('language'),
@@ -68,10 +66,13 @@ class ImportBookView(View):
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
-        if form.is_valid():
-            keyword = form.cleaned_data['keyword']
-            term = request.POST['dropdown']
-            books = self.search(keyword, term)[0]
-            self.add_books(books)
+        if not form.is_valid():
+            return
 
-            return redirect('books:book_list')
+        book_name = form.cleaned_data['book_name']
+        keyword = form.cleaned_data['keyword']
+        term = request.POST['dropdown']
+        books = self.search(book_name, keyword, term)
+        self.add_books(books)
+
+        return redirect('books:book_list')
